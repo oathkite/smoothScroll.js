b6ec1b4a1988efaa777aba966d61fe5401278c74
oathkite <takehito.kita@gmail.com>
2013-05-28 00:29:45 +0900

    bugfix

--- jquery.smoothScroll.js
+++ jquery.smoothScroll.js
@@ -1,6 +1,6 @@
 (function($) {
 	$.fn.smoothScroll = function(options) {
-		$('body').deligate(this, 'click', function(event) {
+		$('body').delegate('a[href^=#]', 'click', function(event) {
 			event.preventDefault();
 			var target = $($(this).attr('href'));
 			$('html, body').animate({
