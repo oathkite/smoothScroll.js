6ebcf298543130550a31cb32ee158403742c3c11
oathkite <takehito.kita@gmail.com>
2013-10-25 08:00:59 +0900

    bug fix

--- jquery.smoothScroll.js
+++ jquery.smoothScroll.js
@@ -1,14 +1,16 @@
 (function($) {
 	$.fn.smoothScroll = function(offset) {
 		offset = offset || 0;
-		$('body').on('click', 'a[href^=#]', function(e) {
-			e.preventDefault();
-			var target = $($(this).attr('href'));
-			$('html, body').animate({
-				scrollTop: target.length ? $($(this).attr('href')).offset().top - offset : 0
-			});
-		})
-	}
+		return this.each(function() {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				var target = $($(this).attr('href')).length ? $($(this).attr('href')).offset().top - offset : 0
+				$('html, body').animate({
+					scrollTop: target
+				});
+			})
+		});
+	};
 })(jQuery);
 $(function() {
 	$('a[href^=#]').smoothScroll();
